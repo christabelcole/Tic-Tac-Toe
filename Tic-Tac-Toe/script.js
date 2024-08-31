0db52ca4188dd062f7ef87cf57f52ddd5524b940
christabelcole <christabelcole1@gmail.com>
2024-08-31 22:36:49 +0100

    JS

---
+++ Tic-Tac-Toe/script.js
@@ -0,0 +1,16 @@
+const statusDisplay = document.querySelector('.game--status');
+
+let gameActive = true;
+
+let currentPlayer = "X";
+
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);

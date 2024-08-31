da42dbc62e8927d11c5c407ebccf59b0ceea6c99
christabelcole <christabelcole1@gmail.com>
2024-08-31 22:40:25 +0100

    js basically all done

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -73,3 +73,16 @@ if (roundDraw) {
 
 handlePlayerChange();
 }
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell')
+               .forEach(cell => cell.innerHTML = "");
+}    

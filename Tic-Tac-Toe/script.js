80937e841a5d1d332db62e87dae44edb36cb78ce
christabelcole <christabelcole1@gmail.com>
2024-08-31 22:38:20 +0100

    js half done

--- Tic-Tac-Toe/script.js
+++ Tic-Tac-Toe/script.js
@@ -14,3 +14,22 @@ statusDisplay.innerHTML = currentPlayerTurn();
 
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
 document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
+function handleCellClick(clickedCellEvent) {   
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(
+      clickedCell.getAttribute('data-cell-index')
+    );
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
